4e0c515deca658a28830a48e5a9cc03de29fef07
Gl <Dimadd11ace@yandex.ru>
2026-02-12 23:00:06 +0500

    added test for multiple browsers in playwright

--- playwright_example.py
+++ playwright_example.py
@@ -4,7 +4,8 @@ from playwright.sync_api import sync_playwright
 playwright = sync_playwright().start()
 
 browser = playwright.chromium.launch(headless=False)
-page = browser.new_page("https://demoqa.com/")
+page = browser.new_page()
+page.goto("https://demoqa.com/")
 time.sleep(10)
 
 browser.close()
