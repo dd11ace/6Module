558c3be1c34c4a55fce5d5b67057b07b1df19b35
Gl <Dimadd11ace@yandex.ru>
2026-02-14 08:18:45 +0500

    Added test for demoqa for add button and registration

--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
 DEFAULT_UI_TIMEOUT = 30000
+
+# links
+DEMOQA_MAIN_LINK = "https://demoqa.com/"
+DEMOQA_TEXT_BOX_LINK = "https://demoqa.com/text-box"
+DEMOQA_WEBTABLES_LINK = "https://demoqa.com/webtables"
--- ui/tests/test_demoqa.py
+++ ui/tests/test_demoqa.py
@@ -1,9 +1,10 @@
 import time
 from playwright.sync_api import Page, expect
+from constants import DEMOQA_TEXT_BOX_LINK, DEMOQA_WEBTABLES_LINK
 
 
 def test_text_box(page: Page):
-    page.goto("https://demoqa.com/text-box")
+    page.goto(DEMOQA_TEXT_BOX_LINK)
 
     page.get_by_role("textbox", name="Full Name").fill("testQa")
     page.get_by_role("textbox", name="name@example.com").fill("test@qa.com")
@@ -21,5 +22,31 @@ def test_text_box(page: Page):
     expect(page.locator("#output #permanentAddress")).to_have_text(
         "Permananet Address :Moscow, Mashkova 1"
     )
+
+    time.sleep(10)
+
+
+def test_add_button_and_registration(page: Page):
+    page.goto(DEMOQA_WEBTABLES_LINK)
+
+    page.get_by_role("button", name="Add").click()
+    page.get_by_text("Registration Form").is_visible()
+    page.get_by_role("textbox", name="First Name").fill("John")
+    page.get_by_role("textbox", name="Last Name").fill("Doe")
+    page.get_by_role("textbox", name="name@example.com").fill("example@example.com")
+    page.get_by_role("textbox", name="Age").fill("30")
+    page.get_by_role("textbox", name="Salary").fill("300000")
+    page.get_by_role("textbox", name="Department").fill("QA")
+
+    page.get_by_role("button", name="Submit").click()
+
+    expect(
+        page.get_by_role(
+            "row", name="First Name Last Name Age Email Salary Department Action"
+        )
+    ).to_be_visible()
+    expect(
+        page.get_by_role("row", name="John Doe 30 example@example.com 300000 QA")
+    ).to_be_visible()
 
     time.sleep(10)
