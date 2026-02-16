a8c6a7cdb46e0c84bfe0549554d1b2334cb971a7
Gl <Dimadd11ace@yandex.ru>
2026-02-16 05:51:06 +0500

    Added alure and pytest.mark for test_cinescope

--- ui/tests/test_cinescope.py
+++ ui/tests/test_cinescope.py
@@ -1,3 +1,5 @@
+import pytest
+import allure
 import time
 from random import randint
 from playwright.sync_api import Page, expect
@@ -5,31 +7,53 @@ from playwright.sync_api import Page, expect
 from constants import CINESCOPE_LOGIN_LINK, CINESCOPE_REGISTER_LINK
 
 
-def test_text_box(page: Page):
-    page.goto(CINESCOPE_REGISTER_LINK)
+@allure.epic("Позитивные UI сценарии для Cinescope")
+@allure.label("qa_name", "Ivan Petrovich")
+@allure.tag("ui", "positive")
+@allure.title("Позитивные UI Тесты для Cinescope")
+@pytest.mark.ui
+@pytest.mark.positive
+class TestCinescope:
+    @allure.feature("Тест функциональности text box")
+    @allure.story("Пользователь может заполнить тестовое поле")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("Провека функциональности text box")
+    @allure.tag("smoke", "input-validation")
+    @pytest.mark.critical
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_text_box(self, page: Page):
+        page.goto(CINESCOPE_REGISTER_LINK)
 
-    page.get_by_role("textbox", name="Имя Фамилия Отчество").fill(
-        "Жмышенко Валерий Альбертович"
-    )
+        with allure.step("Заполнение поля"):
+            page.get_by_role("textbox", name="Имя Фамилия Отчество").fill(
+                "Жмышенко Валерий Альбертович"
+            )
 
-    time.sleep(10)
+        time.sleep(10)
 
+    @allure.feature("Регистрация")
+    @allure.story("Пользователь может зарегестрироваться")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("Проверка заполнения и отправки формы регистрации")
+    @pytest.mark.critical
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_registration(self, page: Page):
+        page.goto(CINESCOPE_REGISTER_LINK)
 
-def test_registration(page: Page):
-    page.goto(CINESCOPE_REGISTER_LINK)
+        user_email = f"test_{randint(1, 9999)}@email.qa"
+        with allure.step("Заполнение полей формы"):
+            page.get_by_role("textbox", name="Имя Фамилия Отчество").fill(
+                "Жмышенко Валерий Альбертович"
+            )
+            page.get_by_role("textbox", name="Email").fill(user_email)
+            page.get_by_role("textbox", name="Пароль", exact=True).fill("qwerty123Q")
+            page.get_by_role("textbox", name="Повторите пароль").fill("qwerty123Q")
+        with allure.step("Отправка формы"):
+            page.get_by_role("button", name="Зарегистрироваться").click()
+        with allure.step("Проверка успешной отправки"):
+            page.wait_for_url(CINESCOPE_LOGIN_LINK)
+            expect(page.get_by_text("Подтвердите свою почту")).to_be_visible()
 
-    user_email = f"test_{randint(1, 9999)}@email.qa"
-
-    page.get_by_role("textbox", name="Имя Фамилия Отчество").fill(
-        "Жмышенко Валерий Альбертович"
-    )
-    page.get_by_role("textbox", name="Email").fill(user_email)
-    page.get_by_role("textbox", name="Пароль", exact=True).fill("qwerty123Q")
-    page.get_by_role("textbox", name="Повторите пароль").fill("qwerty123Q")
-
-    page.get_by_role("button", name="Зарегистрироваться").click()
-
-    page.wait_for_url(CINESCOPE_LOGIN_LINK)
-    expect(page.get_by_text("Подтвердите свою почту")).to_be_visible()
-
-    time.sleep(10)
+        time.sleep(10)
